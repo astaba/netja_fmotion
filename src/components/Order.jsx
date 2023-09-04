f040eca84c3addeaf88dc7d7294d688b57f9a3bd
Astar <bahouidi99@gmail.com>
2023-09-04 13:04:46 +0100

    Apply variants (3) Orchestrating child animations

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,17 +1,53 @@
 import React from "react";
 import { useOutletContext } from "react-router-dom";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      mass: 0.5,
+      damping: 9,
+      when: "beforeChildren",
+      staggerChildren: 0.4,
+    },
+  },
+};
+const childVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+  },
+};
 
 const Order = () => {
   const { pizza } = useOutletContext();
 
   return (
-    <div className="container order">
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      className="container order"
+    >
       <h2>Thank you for your order :)</h2>
-      <p>You ordered a {pizza.base} pizza with:</p>
+      <motion.p variants={childVariants}>
+        You ordered a {pizza.base} pizza with:
+      </motion.p>
       {pizza.toppings.map((topping) => (
-        <div key={topping}>{topping}</div>
+        <motion.div variants={childVariants} key={topping}>
+          {topping}
+        </motion.div>
       ))}
-    </div>
+    </motion.div>
   );
 };
 
