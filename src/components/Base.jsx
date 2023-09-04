fab44eb4e66c8922848ddeb45b1df9963d6d0caf
Astar <bahouidi99@gmail.com>
2023-09-04 01:25:40 +0100

    Initial animation state

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,36 +1,39 @@
-import React from 'react';
-import { Link, useOutletContext } from 'react-router-dom';
+import React from "react";
+import { Link, useOutletContext } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Base = () => {
   const { addBase, pizza } = useOutletContext();
 
-  const bases = ['Classic', 'Thin & Crispy', 'Thick Crust'];
+  const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
   return (
     <div className="base container">
-
       <h3>Step 1: Choose Your Base</h3>
       <ul>
-        {bases.map(base => {
-          let spanClass = pizza.base === base ? 'active' : '';
+        {bases.map((base) => {
+          let spanClass = pizza.base === base ? "active" : "";
           return (
             <li key={base} onClick={() => addBase(base)}>
-              <span className={spanClass}>{ base }</span>
+              <span className={spanClass}>{base}</span>
             </li>
-          )
+          );
         })}
       </ul>
 
       {pizza.base && (
-        <div className="next">
+        <motion.div
+          initial={{ x: "-100vw" }}
+          animate={{ x: 0 }}
+          className="next"
+        >
           <Link to="/toppings">
             <button>Next</button>
           </Link>
-        </div>
+        </motion.div>
       )}
-
     </div>
-  )
-}
+  );
+};
 
 export default Base;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="home container"
+    >
+      <h2>Welcome to Pizza Joint</h2>
       <Link to="/base">
-        <button>
-          Create Your Pizza
-        </button>
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
