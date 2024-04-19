6ea4d8e0926980c5f495eb94f2bb0e73c45ab946
Astar <bahouidi99@gmail.com>
2024-04-20 00:16:18 +0100

    Inherit animation with variants

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,57 @@
 import React from "react";
 import { motion } from "framer-motion";
 
+const svgVariants = {
+  init: {
+    rotate: 0,
+  },
+  anim: {
+    rotate: 360,
+    transition: {
+      duration: 2,
+      delay: 1,
+      repeat: 1,
+      repeatType: "reverse",
+    },
+  },
+};
+
+const pathVariants = {
+  init: { pathLength: 1 },
+  anim: {
+    pathLength: 0,
+    transition: {
+      duration: 2,
+      delay: 1,
+      repeat: 1,
+      repeatType: "reverse",
+    },
+  },
+};
+
 const Header = () => {
   return (
     <header>
       <div className="logo">
-        <svg
+        <motion.svg
+          variants={svgVariants}
+          initial="init"
+          animate="anim"
           className="pizza-svg"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 100 100"
         >
-          <path
+          <motion.path
+            variants={pathVariants}
             fill="none"
             d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
           />
-          <path fill="none" d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z" />
-        </svg>
+          <motion.path
+            variants={pathVariants}
+            fill="none"
+            d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+          />
+        </motion.svg>
       </div>
       <motion.div
         initial={{ y: -250 }}
